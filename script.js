11d67e0e1941ac1b377ac8dd412961a070950f18
noor2025-design <abdun221@newschool.edu>
2026-04-05 18:50:44 -0400

    added google maps url  and address

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ userForm.addEventListener("submit", (event) => {
 
   const cuisineSelection = cuisineSelect.value;
   const priceSelection = priceSelect.value;
-// I needed to capture the users dropdown selections to pass into my function . I learned from this post that you could use getElementById to grab the dropdown and .value to get the selected option, then store each for cuisine and price from stack overflow https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript.
+  // I needed to capture the users dropdown selections to pass into my function . I learned from this post that you could use getElementById to grab the dropdown and .value to get the selected option, then store each for cuisine and price from stack overflow https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript.
   //   console.log(cuisineSelection);
   // add restaurant and dish suggestion to the page based on cuisine selection
 
@@ -28,7 +28,7 @@ let renderItems = (data, cuisineSelection, priceSelection) => {
 
     return;
   }
-//   I noticed the site was still populating results when hitting submit without selecting anything. The return is used inside the function if both fields are empty and found this from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return.
+  //   I noticed the site was still populating results when hitting submit without selecting anything. The return is used inside the function if both fields are empty and found this from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return.
 
   // The `ul` where the items will be inserted.
   let dataList = document.querySelector("#selected-restaurant ul");
@@ -36,9 +36,9 @@ let renderItems = (data, cuisineSelection, priceSelection) => {
   let filteredPrices;
   //   let filteredRestaurants = data;
 
-//   console.log(cuisineSelection);
+  //   console.log(cuisineSelection);
 
-// https://www.w3schools.com/jsref/jsref_tolowercase.asp
+  // https://www.w3schools.com/jsref/jsref_tolowercase.asp
   if (cuisineSelection !== "") {
     filteredCuisines = data.filter(
       (restaurant) => restaurant.cuisine.toLowerCase() === cuisineSelection,
@@ -54,8 +54,8 @@ let renderItems = (data, cuisineSelection, priceSelection) => {
     );
   }
 
-  // I wanted the results to randomly select a different restuarant each time so the site doesnt push out the same resturants when I add more resturants to my json file.  I reviewed this article from stack overflow (https://stackoverflow.com/a/4550514) and a tutor explained it more clearly. The Math.random () generates a decimal number and multiplies it by the array length and Math.floor () rounds it down to a whole number because as I add more restuarants then the array will become longer so filteredPrices.length and filteredCuisines.length will grow. The Math.random is multiplied by however many resturants is in the data.  
-  
+  // I wanted the results to randomly select a different restuarant each time so the site doesnt push out the same resturants when I add more resturants to my json file.  I reviewed this article from stack overflow (https://stackoverflow.com/a/4550514) and a tutor explained it more clearly. The Math.random () generates a decimal number and multiplies it by the array length and Math.floor () rounds it down to a whole number because as I add more restuarants then the array will become longer so filteredPrices.length and filteredCuisines.length will grow. The Math.random is multiplied by however many resturants is in the data.
+
   let selectedRestaurant;
   if (priceSelection !== "" && filteredPrices.length !== 0) {
     selectedRestaurant =
@@ -64,6 +64,8 @@ let renderItems = (data, cuisineSelection, priceSelection) => {
     selectedRestaurant =
       filteredCuisines[Math.floor(Math.random() * filteredCuisines.length)];
   }
+  // building template with selected restaurant
+  const googleUrl = "https://maps.google.com/?q=" + selectedRestaurant.address;
 
   let listItem = `
 				<li class="restaurant-card">
@@ -72,9 +74,10 @@ let renderItems = (data, cuisineSelection, priceSelection) => {
                     <p>${selectedRestaurant.dishType}</p>
                     <p>${selectedRestaurant.location}</p>
                     <p>${selectedRestaurant.description}</p>
+                    <a href="${googleUrl}">Get Directions</a>
                     </li>
                     `;
-
+  // https://stackoverflow.com/questions/1300838/how-to-convert-an-address-into-a-google-maps-link-not-map
   // <p>${selectedRestaurant.price}</p>
   // <p>${selectedRestaurant.cuisine}</p>
   // https://stackoverflow.com/questions/3450593/how-do-i-clear-the-content-of-a-div-using-javascript, https://www.w3schools.com/jsref/prop_html_innerhtml.asp
